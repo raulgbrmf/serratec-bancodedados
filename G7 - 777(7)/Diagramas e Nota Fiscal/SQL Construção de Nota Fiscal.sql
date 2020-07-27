a6f591799d53d9d819650b7349fe4a56b5126375
Mayanaollivier <mayana.ollivier@gmail.com>
2020-07-27 13:31:10 -0300

    Trabalho Final

--- G7 - 777(7)/Diagramas e Nota Fiscal/SQL Construção de Nota Fiscal.sql
+++ G7 - 777(7)/Diagramas e Nota Fiscal/SQL Construção de Nota Fiscal.sql
@@ -3,7 +3,7 @@ from PEDIDO
 inner join cliente on cliente.id = pedido.ID_CLIENTE 
 INNER JOIN PEDIDO_ITENS on pedido_itens.ID_PEDIDO = pedido.ID
 INNER JOIN PRODUTO on produto.ID = pedido_itens.ID_PRODUTO
-WHERE CLIENTE.ID = 1;
+WHERE PEDIDO.ID = 1;
 
 
 SELECT pedido.id as NUMERO_NOTA_FISCAL, cliente.NOME_COMPLETO , endereco.RUA , endereco.NUMERO, endereco.COMPLEMENTO, endereco.CEP, endereco.BAIRRO, endereco.CIDADE, endereco.ESTADO,
@@ -13,4 +13,4 @@ inner join cliente on cliente.id = pedido.ID_CLIENTE
 inner join ENDERECO on endereco.id = cliente.ID_ENDERECO 
 INNER JOIN PEDIDO_ITENS on pedido_itens.ID_PEDIDO = pedido.ID
 INNER JOIN PRODUTO on produto.ID = pedido_itens.ID_PRODUTO
-WHERE CLIENTE.ID = 1
+WHERE PEDIDO.ID = 1
